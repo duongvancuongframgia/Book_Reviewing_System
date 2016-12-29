217955998fe850bf324576989235936fac1c59f4
Duong Van Cuong <duong.van.cuong@framgia.com>
2016-12-30 00:05:34 +0700

    update

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,11 +5,11 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   storage :file
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
